6d249e9c9a9bd73b6505e9be8b84481ea2a18e5d
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-15 22:25:07 +0300

    ✨feat: Add Edit and Delete Button

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -10,12 +10,20 @@ const ContactList = () => {
     .then(data => setContacts(data))
   }
 
-  const listContacts =
+  const tabulateContacts =
       contacts.map(contact => {
         return (
-        <li key={contact.id}>
-          {contact.firstname} {contact.lastname} | {contact.email} | { contact.phone }
-        </li>
+          <>
+            <tr>
+              <td><input type="checkbox" name="" id="" /></td>
+              <td>{contact.firstname}</td>
+              <td>{contact.lastname}</td>
+              <td>{contact.email} </td>
+              <td>{contact.phone} </td>
+              <td><button>Edit</button></td>
+              <td><button>Delete</button></td>
+            </tr>
+          </>
         )
   })
 
@@ -26,9 +34,18 @@ const ContactList = () => {
   return (
     <>
       <h1>ContactList Works!</h1>
-      <ul>
-        {listContacts}
-      </ul>
+      <table>
+        <tr>
+          <th> </th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th> </th>
+          <th> </th>
+        </tr>
+        {tabulateContacts}
+      </table>
     </>
   )
 }
