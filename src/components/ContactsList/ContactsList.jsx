45d52f1931f08e0f3813c3b70c539b61ffddd05a
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-15 20:07:24 +0300

    ✨feat: Fetch and List Contacts from db.json

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import "./ContactsList.css";
 
 const ContactList = () => {
+  const [contacts, setContacts] = useState([])
+
+  const fetchContacts = () => {
+    fetch("http://localhost:3000/contacts")
+    .then(response => response.json())
+    .then(data => setContacts(data))
+  }
+
+  const listContacts =
+      contacts.map(contact => {
+        return (
+        <li key={contact.id}>
+          {contact.firstname} {contact.lastname} | {contact.email} | { contact.phone }
+        </li>
+        )
+  })
+
+  useEffect(()=>{
+    fetchContacts()
+  }, [])
+
   return (
     <>
       <h1>ContactList Works!</h1>
+      <ul>
+        {listContacts}
+      </ul>
     </>
   )
 }
