33cbaec8d2ca4cfacf820a4358f703ee5826e8c4
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-19 15:43:51 +0300

    ✨feat: Add Form to the Edit component for easier editin

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,28 +1,69 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ContactItem = () => {
-  const [updateContact, setUpdateContact] = useState("")
-  const {id} = useParams()
+  const [updateContact, setUpdateContact] = useState({
+    formData: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: ""
+    }
+  })
 
-  const getContact = () => {
+  const {id} = useParams()
+  const navigate = useNavigate()
+
+  const getContacts = () => {
     axios.get(`http://localhost:3000/contacts/${id}`)
     .then(resp => setUpdateContact(resp.data))
   }
 
   useEffect(() => {
-    getContact()
+    getContacts()
   }, [id])
 
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    axios.put(`http://localhost:3000/contacts/${id}`, updateContact)
+    // navigate("/")
+  }
 
   return (
     <>
       <h1>Contact Item Works</h1>
-      <p>Your id is {updateContact.id}</p>
-      <p>Your name is {updateContact.firstname} {updateContact.lastname}</p>
-      <p>Your Email is {updateContact.email}</p>
-      <p>Your Phone is {updateContact.phone}</p>
+      <form onSubmit={handleUpdate}>
+        <label htmlFor="firstname">First Name: </label> 
+        <input 
+          type="text" 
+          value={updateContact.firstname} 
+          onChange={(e) => setUpdateContact({...updateContact, firstname:e.target.value})}
+        />
+        <br />
+        <label htmlFor="lastname">Last Name: </label>
+        <input 
+          type="text" 
+          value={updateContact.lastname}
+          onChange={(e) => setUpdateContact({...updateContact, lastname:e.target.value})} 
+        />
+        <br />
+        <label htmlFor="email">Email: </label>
+        <input 
+          type="text" 
+          value={updateContact.email} 
+          onChange={(e) => setUpdateContact({...updateContact, email:e.target.value})}
+        />
+        <br />
+        <label htmlFor="phone">Phone: </label>
+        <input 
+          type="text"   
+          value={updateContact.phone}
+          onChange={(e) => setUpdateContact({...updateContact, phone:e.target.value})}
+        />
+        <br />
+        <button type="submit">Update</button>
+      </form>
     </>
   )
 }
