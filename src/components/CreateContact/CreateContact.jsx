1df939fe9bbd0d2059e1e153efd20d9a49208da7
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-20 19:03:31 +0300

    ✨feat: Handle Form In Cresubmission to the server

--- src/components/CreateContact/CreateContact.jsx
+++ src/components/CreateContact/CreateContact.jsx
@@ -1,13 +1,22 @@
 import './CreateContact.css'
 import axios from 'axios';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
 const CreateContact = () => {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: ""
+  })
+
 
   const navigate = useNavigate();
 
   const handleCreate = () => {
+    axios.post(` http://localhost:3000/contacts`, formData)
     navigate('/')
   }
   return (
@@ -17,21 +26,33 @@ const CreateContact = () => {
         <label htmlFor="firstname">First Name: </label> 
         <input 
           type="text"
+          onChange={(e) => {
+            setFormData({...formData, firstname:e.target.value})
+          }}
         />
         <br />
         <label htmlFor="lastname">Last Name: </label>
         <input 
           type="text" 
+          onChange={(e) => {
+            setFormData({...formData, lastname:e.target.value})
+          }}
         />
         <br />
         <label htmlFor="email">Email: </label>
         <input 
           type="text" 
+          onChange={(e) => {
+            setFormData({...formData, email:e.target.value})
+          }}
         />
         <br />
         <label htmlFor="phone">Phone: </label>
         <input 
-          type="text"   
+          type="text"  
+          onChange={(e) => {
+            setFormData({...formData, phone:e.target.value})
+          }} 
         />
         <br />
         <button type="submit">Add</button>
