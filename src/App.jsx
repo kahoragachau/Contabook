c46de2cba327f1ff6d926be2fda0eeb1c1196068
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-20 18:18:41 +0300

    ✨feat: Add Route to Create Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ContactList from './components/ContactsList/ContactsList';
 import ContactItem from './components/ContactItem/ContactItem';
+import CreateContact from './components/CreateContact/CreateContact';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<ContactList />} />
           <Route path='/contacts/:id' element={<ContactItem />} />
+          <Route path='/create' element={<CreateContact />} />
         </Routes>
       </BrowserRouter>
     </>
