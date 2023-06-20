bf9964a7bb64342e3b5fb842c6b54e29bc43f680
kahoragachau <joelkahoragachau6@gmail.com>
2023-06-20 18:29:57 +0300

    ✨feat: Add NForm In Create Contact Component

--- src/components/CreateContact/CreateContact.jsx
+++ src/components/CreateContact/CreateContact.jsx
@@ -1,9 +1,41 @@
 import './CreateContact.css'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const CreateContact = () => {
+
+  const navigate = useNavigate();
+
+  const handleCreate = () => {
+    navigate('/')
+  }
   return (
     <>
-    <h1>Create Contact Works!!</h1>
+      <h1>Create Contact Works!!</h1>
+      <form onSubmit={handleCreate}>
+        <label htmlFor="firstname">First Name: </label> 
+        <input 
+          type="text"
+        />
+        <br />
+        <label htmlFor="lastname">Last Name: </label>
+        <input 
+          type="text" 
+        />
+        <br />
+        <label htmlFor="email">Email: </label>
+        <input 
+          type="text" 
+        />
+        <br />
+        <label htmlFor="phone">Phone: </label>
+        <input 
+          type="text"   
+        />
+        <br />
+        <button type="submit">Add</button>
+      </form>
     </>
   )
 }
