ef20d7a4b582b8e759f0a6c579d1b2027ddc83bf
kahoragachau <joelkahoragachau6@gmail.com>
2023-07-13 19:36:32 +0300

    🎨feat: Import Chakra provider to app root

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import ContactList from './components/ContactsList/ContactsList';
-import ContactItem from './components/ContactItem/ContactItem';
-import CreateContact from './components/CreateContact/CreateContact';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
+import ContactList from "./components/ContactsList/ContactsList";
+import ContactItem from "./components/ContactItem/ContactItem";
+import CreateContact from "./components/CreateContact/CreateContact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<ContactList />} />
-          <Route path='/contacts/:id' element={<ContactItem />} />
-          <Route path='/create' element={<CreateContact />} />
-        </Routes>
-      </BrowserRouter>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ContactList />} />
+            <Route path="/contacts/:id" element={<ContactItem />} />
+            <Route path="/create" element={<CreateContact />} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
